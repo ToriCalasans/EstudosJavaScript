88c2106ab860bbb45761269df78fe7426bd8d3bd
ToriCalasans <calasanstori@gmail.com>
2021-06-21 14:56:12 -0300

    novo comit

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+var num = [5,2,6,8]
+console.log("Nosso vetor é " + num)
+num[3]=9
+console.log("Nosso vetor é " + num)
+num[4]=1
+console.log("Nosso vetor é " + num)
+num.push(18)
+console.log("Nosso vetor é " + num)
+num.length
